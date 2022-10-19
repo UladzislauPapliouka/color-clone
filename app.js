f9f2266a331f8002a5860ebb73b9c77cb4851a85
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-10-19 21:34:38 +0300

    Added basic hexColor generator

---
+++ app.js
@@ -0,0 +1,16 @@
+const cols = document.querySelectorAll('.col')
+
+const setRandomColor = () => {
+  cols.forEach(col => {
+    console.log(col)
+  })
+}
+const generateRandomColor = () => {
+  //RGB
+  const hexCodes = "0123456789ABCDEF"
+  let color= ""
+  for(let i = 0; i<6;i++){
+    color+= hexCodes[Math.floor(Math.random()*hexCodes.length)]
+  }
+  return `#${color}`
+}
