cabc9b26be42998168ce791a252c84e305ad18f0
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-10-19 22:57:34 +0300

    Added comments and codestyle

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
+// Gets all colums 
 const cols = document.querySelectorAll('.col')
 
-
+// Update hash with our colors array to share it
 const updateColorHash = (colors = []) => {
   document.location.hash = colors.map((color) => color.toString().substring(1)).join("-")
 
 }
+
+// Get colors arrray from hash
 const getColorsFromHash = () => {
   if(document.location.hash.length > 1){
    return document.location.hash.substring(1).split("-").map(color => `#${color}`)
   }
   return []
 }
+
+// Set random color to colums
 const setRandomColor = (isInitial) => {
   const colors = isInitial ? getColorsFromHash() : []
+
   cols.forEach((col, index) => {
     const isLocked = col.querySelector('i').classList.contains("fa-lock")
     const text =  col.querySelector("h2")
     const button = col.querySelector("button")
+
     if(isLocked){
       colors.push(text.textContent)
       return
     }
+    // Check should we generate new color or not
     const  color = isInitial 
     ? colors[index] 
       ? colors[index] 
       : chroma.random() 
     : chroma.random()
+
     if(!isInitial){
       colors.push(color)
     }
+
     col.style.background =  color
-      text.textContent = color
-      setElementColor(text, color)
-      setElementColor(button, color)
+    text.textContent = color
+    setElementColor(text, color)
+    setElementColor(button, color)
   })
   updateColorHash(colors)
 }
+
+// function to generate random color without libraries
 const generateRandomColor = () => {
   //RGB
   const hexCodes = "0123456789ABCDEF"
@@ -46,15 +58,19 @@ const generateRandomColor = () => {
   return `#${color}`
 }
 
+//Set color to element
 const setElementColor = (element, color) => {
   const luminance = chroma(color).luminance()
   element.style.color = luminance > 0.5 ? '#000' : '#fff'
 }
 
+// Add listener to generate new colors
 document.addEventListener('keydown', event => {
   event.preventDefault()
   event.code === "Space"&& setRandomColor()
 })
+
+// Add listener to work with click event
 document.addEventListener('click', event => {
    const type = event.target.dataset.type
    if(type==="unlock"){
@@ -67,10 +83,10 @@ document.addEventListener('click', event => {
     copyToClipboard(event.target.textContent)
    }
 })
-
+// Function to copy to clipboard
 const copyToClipboard = (text) => {
   return navigator.clipboard.writeText(text)
 }
 
-
+// First start to generate colors or get them from hash
 setRandomColor(true)
